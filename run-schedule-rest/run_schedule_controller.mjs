4ff8d3349bafed0fa585b47836c245c68c6e144f
Alli <allisonglittle@gmail.com>
2022-04-20 15:08:47 -0700

    Initial Create functionality in controller

--- run-schedule-rest/run_schedule_controller.mjs
+++ run-schedule-rest/run_schedule_controller.mjs
@@ -1,4 +1,26 @@
+import * as workouts from './run_schedule_model.mjs';
 import express from 'express';
-const app = express();
 
 const PORT = 3000;
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+
+/**
+ * Create new workout with input parameters in request body
+ */
+ app.post('/workout', (req, res) => {
+    workouts.createWorkout(req.body.name, req.body.day, req.body.distance, req.body.duration, req.body.target, req.body.pace)
+        .then(Workout => {
+            res.status(201).json(Workout);
+        })
+        .catch(error => {
+            console.error(error);
+            // In case of an error, send back status code 400.
+            res.status(400).json({ Error: 'Request failed' });
+        });
+});
