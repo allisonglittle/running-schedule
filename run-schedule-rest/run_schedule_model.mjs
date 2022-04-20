2eadafefa3685e30d594b7a8f9c71394cf1cca02
Alli <allisonglittle@gmail.com>
2022-04-20 10:19:17 -0700

    Connect to database

--- run-schedule-rest/run_schedule_model.mjs
+++ run-schedule-rest/run_schedule_model.mjs
@@ -1,2 +1,19 @@
 // Get the mongoose object
 import mongoose from 'mongoose';
+
+// Identify database run_schedule_db in the MongoDB server running locally on port 27017
+mongoose.connect(
+    'mongodb://localhost:27017/run_schedule_db',
+    { useNewUrlParser: true }
+);
+
+// Connect to to the database
+const db = mongoose.connection;
+
+// The open event is called when the database connection successfully opens
+db.once('open', () => {
+    console.log('Successfully connected to MongoDB using Mongoose!');
+});
+
+// Have mongoose create indexes, this helps with faster querying
+mongoose.set('useCreateIndex', true);
