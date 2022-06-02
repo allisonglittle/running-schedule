8c6ebfecf9f13185a91c5d71daf296ad41226dee
Alli <allisonglittle@gmail.com>
2022-06-01 18:29:42 -0700

    Removed old toggle buttons

--- run-schedule-ui/src/components/WeeklyDistance.js
+++ run-schedule-ui/src/components/WeeklyDistance.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ToggleButton from 'react-bootstrap/Button';
 import Button from 'react-bootstrap/Button';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function WeeklyDistance({ distance, sumWorkouts, units, setUnits }) {
     // For toggling radio button state
     const [isLoading, setLoading] = useState(false);
 
-    const radios = [{ value: "miles" }, { value: "km" }];
-
-    const updateDistance = async (units) => {
+    const updateDistance = (units) => {
         setLoading(true);
         
         if (isLoading) {
@@ -27,21 +24,6 @@ function WeeklyDistance({ distance, sumWorkouts, units, setUnits }) {
             <p>Weekly Distance:</p>
             <p>{distance.toFixed(2)} {units}</p>
             <ButtonGroup>
-                {/* {radios.map((radio, idx) => (
-                    <ToggleButton
-                        key={idx}
-                        id={`radio-${idx}`}
-                        type="radio"
-                        variant="secondary"
-                        name="radio"
-                        value={radio.value}
-                        checked={units === radio.value}
-                        disabled={isLoading}
-                        onClick={() => updateDistance(radio.value)}
-                    >
-                        {isLoading ? 'Loading…' : radio.value}
-                    </ToggleButton>
-                ))} */}
                 <Button onClick={() => updateDistance("miles")}>Miles</Button>
                 <Button onClick={() => updateDistance("km")}>Km</Button>
             </ButtonGroup>
